2f3dd5af1525208cf8584196c50d508cccedae9c
Micanga <matheus.aparecido.alves@usp.br>
2020-03-10 14:05:27 -0300

    working on plots

--- ex1/naive_classifier.py
+++ ex1/naive_classifier.py
@@ -1,4 +1,6 @@
 from math import exp
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 import re
 
@@ -48,16 +50,23 @@ with open('Aula2-exec1.csv') as inputfile:
 			v3.append(int(data[2]))
 			number_of_examples += 1
 
+
+print "# Plotting dataset"
+plt.scatter(v1,v2)
+plt.show()
+
 # 2. Initializing the Neuron
 neuron = Neuron(nweights=2)
 
 # 3. Analysing the best weight and bias to the proposed problem
 print "# Training"
-threshold_result = []
-linear_result = []
-sigmoid_result = []
+threshold_result, linear_result, sigmoid_result = [], [], []
+t_class, l_class, s_class = [], [], []
+
+counter = 1
+max_c = len(np.arange(-5,5,0.5))
 for bias in np.arange(-5,5,0.5):
-	print "->",bias
+	print "-> progress: "+str(100*float(counter)/float(max_c))+'%'
 	for w1 in np.arange(-5,5,0.5):
 		for w2 in np.arange(-5,5,0.5):
 			# a. initialising the errors
@@ -74,36 +83,45 @@ for bias in np.arange(-5,5,0.5):
 				neuron.bias = bias
 
 				# e. calculating the error
-				threshold_error += abs(round(neuron.threshold_classify(x))-y)
-				linear_error += abs(round(neuron.linear_classify(x))-y)
-				sigmoid_error += abs(round(neuron.sigmoid_classify(x))-y)
+				t_class.append(round(neuron.threshold_classify(x)))
+				l_class.append(round(neuron.linear_classify(x)))
+				s_class.append(round(neuron.sigmoid_classify(x)))
+
+				threshold_error += abs(t_class[-1]-y)
+				linear_error += abs(l_class[-1]-y)
+				sigmoid_error += abs(s_class[-1]-y)
 			
 			threshold_result.append([bias,w1,w2,threshold_error])
 			linear_result.append([bias,w1,w2,linear_error])
 			sigmoid_result.append([bias,w1,w2,sigmoid_error])
+	counter += 1
 
 # 4. Getting the best bias and weights to the problem for each activation func
 min_error, t_best_config = 99999999, None
 for config in threshold_result:
 	if config[3] < min_error:
 		min_error = config[3]
-		best_config = config
-print "Threshold:",best_config
+		t_best_config = config
+print "Threshold:",t_best_config
 
 min_error, l_best_config = 99999999, None
 for config in linear_result:
 	if config[3] < min_error:
 		min_error = config[3]
-		best_config = config
-print "Linear:",best_config
+		l_best_config = config
+print "Linear:",l_best_config
 
 min_error, s_best_config = 99999999, None
 for config in sigmoid_result:
 	if config[3] < min_error:
 		min_error = config[3]
-		best_config = config
-print "Sigmoid:",best_config
+		s_best_config = config
+print "Sigmoid:",s_best_config
 
 # 4. Plotting the result
-import matplotlib.pyplot as plt
-plt.plot(v1,v2)
+plt.subplot(1, 3, 1)
+colors = ['r' if t_class[i] == 1 else 'b' for i in range(len(v1))]
+plt.scatter(v1,v2,color=colors)
+plt.show()
+
+# That's all folks :)
